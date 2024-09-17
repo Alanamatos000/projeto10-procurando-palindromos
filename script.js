c500a9b1ff69a464c4302eff58107863f9bbbd0a
Alanamatos000 <alanamatos44731@gmail.com>
2024-09-16 22:14:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Exibir título do programa
+console.log("PROCURANDO PALÍNDROMOS");
+
+// Solicitar ao usuário que informe uma palavra
+let word = prompt("Informe uma palavra para verificar se é um palíndromo:");
+
+// Transformar a palavra em minúsculas para garantir que a comparação ignore maiúsculas e minúsculas
+word = word.toLowerCase();
+
+// Reverter a palavra
+let reversedWord = word.split('').reverse().join('');
+
+// Verificar se a palavra é um palíndromo
+if (word === reversedWord) {
+    console.log("A palavra informada é um palíndromo!");
+} else {
+    console.log(`A palavra informada NÃO é um palíndromo.\nPalavra normal: ${word}\nPalavra invertida: ${reversedWord}`);
+}
